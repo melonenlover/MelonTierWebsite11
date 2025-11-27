1fcee601c3935d0073d1f054512ccaafddb76cea
zimmj88433 <50304825-zimmj88433@users.noreply.replit.com>
2025-11-27 19:09:11 +0000

    Restored to '95e660f3f96f3f3fc1116058702d58dcf54cd6da'
    Replit-Restored-To: 95e660f3f96f3f3fc1116058702d58dcf54cd6da

--- server/db.ts
+++ server/db.ts
@@ -5,13 +5,11 @@ import * as schema from "@shared/schema";
 
 neonConfig.webSocketConstructor = ws;
 
-const databaseUrl = process.env.DATABASE_URL || process.env.WEBSITE_DATABASE_URL;
-
-if (!databaseUrl) {
+if (!process.env.DATABASE_URL) {
   throw new Error(
     "DATABASE_URL must be set. Did you forget to provision a database?",
   );
 }
 
-export const pool = new Pool({ connectionString: databaseUrl });
+export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle({ client: pool, schema });
